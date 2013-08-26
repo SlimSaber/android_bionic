d8a3f2629806406bdbb3054c4632462e9d084b3a
fusionjack <fusionjack@fusionjack.com>
2013-08-26 18:59:48 +0200

    Add ARMv7 optimized string handling routines and memcpy from cortex-strings
    Change-Id: Ib10f45d36a3bf61e4e0114305a58f4c7cfe5a843

--- libc/arch-arm/cortex-a9/cortex-a9.mk
+++ libc/arch-arm/cortex-a9/cortex-a9.mk
@@ -1,5 +1,6 @@
-$(call libc-add-cpu-variant-src,MEMCPY,arch-arm/cortex-a9/bionic/memcpy.S)
-$(call libc-add-cpu-variant-src,MEMSET,arch-arm/cortex-a9/bionic/memset.S)
+$(call libc-add-cpu-variant-src,MEMCPY,arch-arm/bionic/memcpy.a9.S)
+$(call libc-add-cpu-variant-src,MEMCHR,arch-arm/armv7/memchr.S)
+$(call libc-add-cpu-variant-src,MEMSET,arch-arm/armv7/memset.S)
 $(call libc-add-cpu-variant-src,STRCAT,arch-arm/cortex-a9/bionic/strcat.S)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/cortex-a9/bionic/strcmp.S)
 ifeq ($(ARCH_ARM_HAVE_NEON),true)
@@ -8,7 +9,7 @@ else
 $(call libc-add-cpu-variant-src,MEMMOVE,bionic/memmove.c.arm)
 $(call libc-add-cpu-variant-src,BCOPY,string/bcopy.c.arm)
 endif
-$(call libc-add-cpu-variant-src,STRCPY,arch-arm/cortex-a9/bionic/strcpy.S)
-$(call libc-add-cpu-variant-src,STRLEN,arch-arm/cortex-a9/bionic/strlen.S)
+$(call libc-add-cpu-variant-src,STRCPY,arch-arm/armv7/strcpy.c)
+$(call libc-add-cpu-variant-src,STRLEN,arch-arm/armv7/strlen.S)
 
 #include bionic/libc/arch-arm/generic/generic.mk
