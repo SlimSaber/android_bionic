67da57239d0c2993c74ae0fbfca131572c8c77cc
fusionjack <fusionjack@fusionjack.com>
2013-08-27 19:42:43 +0200

    Removed memchr and memset ARMv7 optimization
    Change-Id: I658368df6ace7ecdeb3919ec42a6ba7913ba0d45

--- libc/arch-arm/cortex-a9/cortex-a9.mk
+++ libc/arch-arm/cortex-a9/cortex-a9.mk
@@ -1,6 +1,5 @@
 $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/bionic/memcpy.a9.S)
-$(call libc-add-cpu-variant-src,MEMCHR,arch-arm/armv7/memchr.S)
-$(call libc-add-cpu-variant-src,MEMSET,arch-arm/armv7/memset.S)
+$(call libc-add-cpu-variant-src,MEMSET,arch-arm/cortex-a9/bionic/memset.S)
 $(call libc-add-cpu-variant-src,STRCAT,arch-arm/cortex-a9/bionic/strcat.S)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/cortex-a9/bionic/strcmp.S)
 ifeq ($(ARCH_ARM_HAVE_NEON),true)
